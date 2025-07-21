efb0485a9e7bfea6e7e063ec5bf73514a823144e
Suchita Kulkarni <suchita.kulkarni@gmail.com>
2025-07-21 09:10:32 +0200

    removing non-functional project

--- GenAI_field_summariser_agentic_MLOps/src/pipeline/models/__init__.py
+++
@@ -1,7 +0,0 @@
-# src/pipeline/models/__init__.py
-"""Models package initialization"""
-
-from .paper import Paper
-from .schemas import PipelineConfig, CollectorConfig, AnalyzerConfig
-
-__all__ = ["Paper", "PipelineConfig", "CollectorConfig", "AnalyzerConfig"]
