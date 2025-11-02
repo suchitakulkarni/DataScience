cf1421588dcbf7c762b609f733ae9404e0fe7635
Suchita Kulkarni <suchita.kulkarni@gmail.com>
2025-11-02 10:21:56 +0100

    deleting physics VAE

--- Physics_VAE/config.py
+++
@@ -1,8 +0,0 @@
-import os
-import torch
-
-results_dir = "results"
-os.makedirs(results_dir, exist_ok=True)
-
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
-print(f"Using device: {device}")
